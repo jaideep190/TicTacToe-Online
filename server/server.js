1e2b40bf24b9dcdd601dfa442933251629113384
jaideep190 <ajaiajai710@gmail.com>
2024-07-18 22:44:02 +0530

    Endpoints updated

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://tic-tac-toe-online-six.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
